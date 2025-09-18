e7cfceda2093e41eb96ff501be9c0df71fb10428
Michael Clark <michaeljclark@mac.com>
2025-09-18 14:29:11 +1200

    fix data file path location and add missing memset

--- util/demo.c
+++ util/demo.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#ifdef _WIN32
+#define PATH_MAX 1024
+#endif
 
 #include "lv_ephem.h"
+#include "lv_osutil.h"
 
-static const char* ephem_bin = "build/data/DE440Coeff.bin";
-
+static const char* ephem_bin = "resources/data/DE440Coeff.bin";
 
 static void de440_print_planet(const char *name, double *r)
 {
-    printf("%20s: (%10.3le,%10.3le,%10.3le)\n", name, r[0], r[1], r[2]);
+    printf("%22s: (%10.3le,%10.3le,%10.3le)\n", name, r[0], r[1], r[2]);
 }
 
 void de440_print_ephemeris(ephem_ctx *ctx, double jd)
 {
     size_t row = de440_find_row(ctx, jd);
 
-    printf("%20s: %10.2lf\n", "MJD", jd);
+    printf("%22s: %10.2lf\n", "MJD", jd);
 
-    for (size_t oid = 0; oid < ephem_id_Last; oid++) {
+    for (size_t oid = 1; oid < ephem_id_Last; oid++) {
         double obj[3];
         de440_ephem_obj(ctx, jd, row, oid, obj);
         de440_print_planet(de440_object_name(oid), obj);
@@ -27,8 +33,17 @@ void de440_print_ephemeris(ephem_ctx *ctx, double jd)
 void demo(double jd)
 {
     ephem_ctx ctx;
+    char ephem_bin_rsrc[PATH_MAX];
 
-    de440_create_ephem(&ctx, ephem_bin);
+    if (get_resource_path(ephem_bin_rsrc, sizeof(ephem_bin_rsrc),
+        ephem_bin) != 0)
+    {
+        fprintf(stderr, "demo: failed to locate resources\n");
+        exit(1);
+    }
+
+    memset(&ctx, 0, sizeof(ctx));
+    de440_create_ephem(&ctx, ephem_bin_rsrc);
     de440_print_ephemeris(&ctx, jd);
     de440_destroy_ephem(&ctx);
 }
